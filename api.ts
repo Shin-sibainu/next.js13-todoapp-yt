c0b419015252d6325be2e78892add792652dbded
Shin-sibainu <shincode0712@gmail.com>
2023-07-18 15:30:55 +0900

    done

--- api.ts
+++ api.ts
@@ -3,7 +3,8 @@ import { Task } from "@/types/tasks";
 const baseUrl = "http://localhost:3001";
 
 export const getAllTodos = async (): Promise<Task[]> => {
-  const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" }); //getserversideprops
+  // const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" }); //getserversideprops
+  const res = await fetch(`${baseUrl}/tasks`, { next: { revalidate: 30 } }); //getserversideprops
   const todos = await res.json();
   return todos;
 };
@@ -18,4 +19,30 @@ export const addTodo = async (todo: Task): Promise<Task> => {
   });
   const newTodo = await res.json();
   return newTodo;
+};
+
+export const updateTodo = async (
+  id: string,
+  newText: string
+): Promise<Task> => {
+  const res = await fetch(`${baseUrl}/tasks/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: newText }),
+  });
+  const updatedTodo = await res.json();
+  return updatedTodo;
+};
+
+export const deleteTodo = async (id: string): Promise<Task> => {
+  const res = await fetch(`${baseUrl}/tasks/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const deleteTodo = await res.json();
+  return deleteTodo;
 };
--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -1,19 +1,82 @@
+"use client";
+
 import { Task } from "@/types/tasks";
+import { useEffect, useRef, useState } from "react";
+import { deleteTodo, updateTodo } from "../../../api";
+import { useRouter } from "next/navigation";
+import { PencilAltIcon, SaveIcon, TrashIcon } from "@heroicons/react/solid";
 
 interface TaskProps {
   task: Task;
 }
 
 export default function Todo({ task }: TaskProps) {
+  const router = useRouter();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTaskText, setEditedTaskText] = useState(task.text);
+
+  useEffect(() => {
+    if (isEditing) {
+      // isEditingがtrueならinputにフォーカスを当てる
+      inputRef.current?.focus();
+    }
+  }, [isEditing]);
+
+  const handleEditButtonClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveButtonClick = async () => {
+    await updateTodo(task.id, editedTaskText);
+    setIsEditing(false);
+    router.refresh();
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedTaskText(event.target.value);
+  };
+
+  const handleDelete = async () => {
+    await deleteTodo(task.id);
+    router.refresh();
+  };
+
   return (
     <li
       key={task.id}
       className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
     >
-      <span className="text-gray-700">{task.text}</span>
-      <div>
-        <button className="text-green-400 hover:text-green-700 mr-3">✒</button>
-        <button className="text-red-500 hover:text-red-700">✖</button>
+      {isEditing ? (
+        <input
+          ref={inputRef}
+          value={editedTaskText}
+          onChange={handleInputChange}
+          className="mr-2 py-1 px-2 rounded border-gray-400 border"
+        />
+      ) : (
+        <span className="text-gray-700">{task.text}</span>
+      )}
+      <div className="flex">
+        {isEditing ? (
+          <SaveIcon
+            onClick={handleSaveButtonClick}
+            className="h-5 w-5 text-blue-500 hover:text-blue-700 cursor-pointer mr-3"
+          />
+        ) : (
+          <>
+            <PencilAltIcon
+              onClick={handleEditButtonClick}
+              className="h-5 w-5 text-green-400 hover:text-green-700 cursor-pointer mr-3"
+            />
+          </>
+        )}
+        <TrashIcon
+          onClick={handleDelete}
+          className="h-5 w-5 text-red-500 hover:text-red-700 cursor-pointer"
+        />
       </div>
     </li>
   );
